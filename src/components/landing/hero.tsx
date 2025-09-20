5c055a501c7eebe3382cd5c179f9a83e166dbf14
SuryaNesanuru <nesanurusuryavaraprasad@gmail.com>
2025-09-20 21:48:01 +0530

    fixing error in button comp

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from 'framer-motion';
-import { Button } from '../../ui/button';
+import { Button } from '../ui';
 import Link from 'next/link';
 import { SearchBar } from './search';
 
